da3fe3de78d8a6d0580a192dba4ab585e833545f
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-06-30 08:33:22 -0400

    Funciones al boton

--- Mokepon/script.js
+++ Mokepon/script.js
@@ -1,6 +1,23 @@
-function seleccionarMascotaJugador(params) {
-    alert("Ya funciona")
+function iniciarJuego(params) {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(params) {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked) {
+        alert('Seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('Seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('Seleccionaste a Ratigueya')
+    } else {
+        alert('No has elegido ninguna mascota')
+    }
+}
+
+
+window.addEventListener('load', iniciarJuego)
