a0728774cbc1a7fb715cdb6de52e951ec6f2b5de
Franlex Ramirez <franlex.ramirez@gmail.com>
2023-07-09 08:47:07 -0400

    Contador de vidas

--- Mokepon/script.js
+++ Mokepon/script.js
@@ -1,5 +1,7 @@
 let ataqueJugador
 let ataqueEnemigo
+let vidasJugador = 3
+let vidasEnemigo = 3
 
 function iniciarJuego(params) {
     let botonMascotaJugador = document.getElementById('boton-mascota')
@@ -73,16 +75,27 @@ function ataqueAleatorioEnemigo() {
 }
 
 function combate() {
+    let spanVidasJugador = document.getElementById('vidas-jugador')
+    let spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
     if(ataqueEnemigo == ataqueJugador){
         crearMensaje("Empate")
     } else if(ataqueJugador== 'FUEGO' && ataqueEnemigo== 'TIERRA') {
         crearMensaje("Ganaste!! 🎉🎊")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     } else if (ataqueJugador== 'AGUA' && ataqueEnemigo== 'FUEGO') {
         crearMensaje("Ganaste!! 🎉🎊")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     } else if (ataqueJugador== 'TIERRA' && ataqueEnemigo== 'AGUA') {
         crearMensaje("Ganaste!! 🎉🎊")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
     } else {
         crearMensaje("Perdiste")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
     }
 }
 
